8af73fe990014215f0975d7f613b9fd9eb01d174
Khlidbg78dem <khalidjj78200@icloud.com>
2024-02-06 20:44:19 +0100

    fonction afficher carte fonctionnel + les valeur 0 modifier en caractere speciale +fonction effacer fait mais pas totalement fonctionnel

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@ import Model.map;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] test = map.generate();
-
-        System.out.println(test);
+        // Génère la carte
+        int[][] generatedMap = map.generate();
+        //Remplace les zéros par un caractère spécial
+        map.effacerZero(generatedMap);
+        // Affiche la carte avec les zéros remplacés
+        map.afficherMap(generatedMap);
+        map.effacerMap(generatedMap);
     }
 }
--- src/Model/map.java
+++ src/Model/map.java
@@ -1,12 +1,8 @@
 package Model;
 
-import java.awt.Graphics;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-import java.util.Scanner;
 public class map {
-
+    // Variable de contrôle pour activer/désactiver l'affichage de la carte
+    private static boolean active_affichage = true;
     public static int[][] generate() {
 
 
@@ -28,14 +24,58 @@ public class map {
         //je vais parcourir la matrice présente juste au dessus en faisant une boucle dans une boucle
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat.length + 1; j++) {
-                System.out.print(mat[i][j] + "\t");
+
             }
-            // permet d afficher la matrice de façon ordonnes
+
         }
             return mat;
     }
 
 
+    public static void effacerZero(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] == 0) {
+                    // je vais Remplace les zéros par le caractère spécial "□"
+                    mat[i][j] = -1;
+                }
+            }
+        }
     }
+
+    public static void afficherMap(int[][] mat) {
+        if (!active_affichage) {
+            return; // Si l'affichage est désactivé, quitte la méthode sans rien afficher
+        }
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] == -1) {
+                    // Remplace -1 par le caractère spécial pour la case vide
+                    System.out.print("□" + "\t");
+                } else {
+                    System.out.print(mat[i][j] + "\t");
+                }
+            }
+            // Nouvelle ligne pour chaque ligne de la matrice
+            System.out.println();
+        }
+    }
+
+    public static void effacerMap(int[][] generatedMap) {
+        // Réinitialise les valeurs de la carte à 0
+        // Réinitialise les valeurs de la carte à 0 (ou à la valeur que vous considérez comme "vide")
+        for (int i = 0; i < generatedMap.length; i++) {
+            for (int j = 0; j < generatedMap[i].length; j++) {
+                generatedMap[i][j] = 0; // Remplacez 0 par la valeur que vous considérez comme "vide"
+            }
+        }
+        // Désactive l'affichage de la carte
+        active_affichage = false;
+        System.out.println("La carte a été effacée.");
+    }
+
+}
+
+
 
 
